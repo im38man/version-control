f7b2b6f4a627c4631240383dbce5c61b8c4b5731
im38man <149188276+im38man@users.noreply.github.com>
2026-09-10 00:22:32 +0700

    Add files via upload

---
+++ ManPhotos/data.js
@@ -0,0 +1,75 @@
+// data.js - Data Dummy Galeri Foto Firman Hidayat
+const dataGaleriFirman = [
+  {
+    title: "Keindahan Alam Pegunungan",
+    imgUrl: "https://picsum.photos/id/1015/800/600",
+    lightboxUrl: "https://picsum.photos/id/1015/1200/800",
+    category: "Alam",
+    location: "Gunung Bromo, Jawa Timur",
+    date: "12 Agustus 2025",
+    detailUrl: "gallery-single.html"
+  },
+  {
+    title: "Kabut Pagi di Hutan Pinus",
+    imgUrl: "https://picsum.photos/id/1016/800/600",
+    lightboxUrl: "https://picsum.photos/id/1016/1200/800",
+    category: "Alam",
+    location: "Lembang, Bandung",
+    date: "15 September 2025",
+    detailUrl: "gallery-single.html"
+  },
+  {
+    title: "Senja di Tepi Pantai",
+    imgUrl: "https://picsum.photos/id/1018/800/600",
+    lightboxUrl: "https://picsum.photos/id/1018/1200/800",
+    category: "Perjalanan",
+    location: "Pantai Kuta, Bali",
+    date: "20 Oktober 2025",
+    detailUrl: "gallery-single.html"
+  },
+  {
+    title: "Arsitektur Gedung Modern",
+    imgUrl: "https://picsum.photos/id/1019/800/600",
+    lightboxUrl: "https://picsum.photos/id/1019/1200/800",
+    category: "Arsitektur",
+    location: "Jakarta Pusat",
+    date: "05 November 2025",
+    detailUrl: "gallery-single.html"
+  },
+  {
+    title: "Potret Ekspresi Wajah",
+    imgUrl: "https://picsum.photos/id/1025/800/600",
+    lightboxUrl: "https://picsum.photos/id/1025/1200/800",
+    category: "Potret Manusia",
+    location: "Studio Firman Hidayat, Bandung",
+    date: "10 Desember 2025",
+    detailUrl: "gallery-single.html"
+  },
+  {
+    title: "Aksi Satwa Liar",
+    imgUrl: "https://picsum.photos/id/1043/800/600",
+    lightboxUrl: "https://picsum.photos/id/1043/1200/800",
+    category: "Hewan",
+    location: "Taman Safari, Bogor",
+    date: "02 Januari 2026",
+    detailUrl: "gallery-single.html"
+  },
+  {
+    title: "Kompetisi Olahraga Lari",
+    imgUrl: "https://picsum.photos/id/1049/800/600",
+    lightboxUrl: "https://picsum.photos/id/1049/1200/800",
+    category: "Olahraga",
+    location: "Stadion Siliwangi, Bandung",
+    date: "14 Februari 2026",
+    detailUrl: "gallery-single.html"
+  },
+  {
+    title: "Perjalanan Menjelajah Kota",
+    imgUrl: "https://picsum.photos/id/1050/800/600",
+    lightboxUrl: "https://picsum.photos/id/1050/1200/800",
+    category: "Perjalanan",
+    location: "Malioboro, Yogyakarta",
+    date: "01 Maret 2026",
+    detailUrl: "gallery-single.html"
+  }
+];
